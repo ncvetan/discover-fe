3d6a841a3a2df8e784dab6c67048a1ccff66318e
Noah <noahcvetanovski@gmail.com>
2022-12-12 22:44:05 -0500

    Testing for deployment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: `${process.env.REACT_APP_SERVER_URI}`,
+    baseURL: 'https://discover-be-west.onrender.com',
 });
 
 export async function getPlaces(category, pageParam = 1, options = {}) {
