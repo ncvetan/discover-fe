5d96865a072e51b61f1253ad60acd0815d87cc00
Noah <noahcvetanovski@gmail.com>
2022-12-13 09:28:47 -0500

    Disable dev tools in prod

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import './index.css';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
+const environment = process.env.REACT_APP_NODE_ENV;
+
+if (environment === 'production') disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
